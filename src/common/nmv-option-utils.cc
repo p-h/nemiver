ee8625899d92996fc4b79569b69dcb4a820685be
Dodji Seketeli <dodji@src.gnome.org>
2006-07-31 12:28:01 +0000

    lundi 31 juillet 2006, 14:25:41 (UTC+0200) Dodji Seketeli
    forgot to add these two files: src/common/nmv-option-utils.cc src/common/nmv-option-utils.h
    
    
    svn path=/trunk/; revision=24

---
+++ src/common/nmv-option-utils.cc
@@ -0,0 +1,57 @@
+/*
+ *This file is part of the Nemiver project
+ *
+ *Nemiver is free software; you can redistribute
+ *it and/or modify it under the terms of
+ *the GNU General Public License as published by the
+ *Free Software Foundation; either version 2,
+ *or (at your option) any later version.
+ *
+ *Nemiver is distributed in the hope that it will
+ *be useful, but WITHOUT ANY WARRANTY;
+ *without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *See the GNU General Public License for more details.
+ *
+ *You should have received a copy of the
+ *GNU General Public License along with Goupil;
+ *see the file COPYING.
+ *If not, write to the Free Software Foundation,
+ *Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *See COPYRIGHT file copyright information.
+ */
+
+#include "nmv-option-utils.h"
+
+namespace nemiver {
+namespace options_utils {
+
+void
+option_desc_to_option (OptionDesc &a_desc,
+                       Glib::OptionEntry &a_entry)
+{
+    Glib::OptionEntry entry ;
+    entry.set_long_name (a_desc.long_name ()) ;
+    entry.set_short_name (a_desc.short_name ()) ;
+    entry.set_description (a_desc.description ()) ;
+    entry.set_arg_description (a_desc.arg_description ()) ;
+    entry.set_flags (a_desc.flags ()) ;
+}
+
+void
+append_options_to_group (OptionDesc *a_descs,
+                         int a_number_of_options,
+                         Glib::OptionGroup &a_group)
+{
+    Glib::OptoinEntry entry ;
+
+    for (int i = 0 ; i < a_number_of_options ; ++i) {
+        option_desc_to_option (a_descs[i], entry) ;
+        a_group.add_entry (entry) ;
+    }
+}
+
+}//end namespace options_utils
+}//end namespace nemiver
+
---
+++ src/common/nmv-option-utils.h
@@ -0,0 +1,86 @@
+/*
+ *This file is part of the Nemiver project
+ *
+ *Nemiver is free software; you can redistribute
+ *it and/or modify it under the terms of
+ *the GNU General Public License as published by the
+ *Free Software Foundation; either version 2,
+ *or (at your option) any later version.
+ *
+ *Nemiver is distributed in the hope that it will
+ *be useful, but WITHOUT ANY WARRANTY;
+ *without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *See the GNU General Public License for more details.
+ *
+ *You should have received a copy of the
+ *GNU General Public License along with Goupil;
+ *see the file COPYING.
+ *If not, write to the Free Software Foundation,
+ *Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *See COPYRIGHT file copyright information.
+ */
+#ifndef __NMV_OPTION_UTILS_H__
+#define __NMV_OPTION_UTILS_H__
+
+#include <glibmm.h>
+#include "nmv-ustring.h"
+
+using nemiver::common::UString ;
+
+namespace nemiver {
+namespace options_utils {
+
+class OptionDesc {
+    UString m_long_name ;
+    UString m_short_name ;
+    UString m_short_name ;
+    UString m_description ;
+    UString m_arg_description ;
+    enum Glib::OptionEntry::Flags m_flags ;
+
+public:
+    OptionDesc () :
+        m_flags (0)
+    {}
+
+    OptionDesc (const UString &a_long_name,
+                const UString &a_short_name,
+                const UString &a_description,
+                const UString &a_arg_description,
+                const Glib::OptionEntry::Flags a_flags) :
+        m_long_name (a_long_name),
+        m_short_name (a_short_name),
+        m_description (a_description),
+        m_arg_description (a_description),
+        m_flags (a_flags)
+    {}
+
+    const UString& long_name () {return m_long_name;}
+    void long_name (const UString &a_in) {m_long_name = a_in;}
+
+    const UString& short_name () {return m_short_name;}
+    void short_name (const UString &a_in) {m_short_name = a_in;}
+
+    const UString& description () {return m_description;}
+    void description (const UString &a_in) {m_description = a_in;}
+
+    const UString& arg_description () {return m_arg_description;}
+    void arg_description (const UString &a_in) {m_arg_description = a_in;}
+
+    Glib::OptionEntry::Flags flags () {return m_flags;}
+    void flags (Glib::Options::Flags a_in) {m_flags = a_in;}
+}; //end class OptionDesc
+
+void option_desc_to_option (OptionDesc &a_desc,
+                            Glib::Option &a_option) ;
+
+void append_options_to_group (OptionDesc *a_descs,
+                              int a_number_of_options,
+                              Glib::OptionGroup &a_group) ;
+}//end namespace nemiver
+}//end namespace options_utils
+
+#endif //__NMV_OPTION_UTILS_H__
+
