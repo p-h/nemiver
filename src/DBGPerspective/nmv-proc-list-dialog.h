3a031bb35c4c1465ded906486bf2c3f1b0304e93
Dodji Seketeli <dodji@src.gnome.org>
2006-08-09 16:27:33 +0000

    mercredi 9 août 2006, 18:37:32 (UTC+0200) Dodji Seketeli
    	* src/DBGPerspective/nmv-proc-list-dialog.cc,h: started to work on the
    	proc list dialog. Nothing works for now.
    
    
    svn path=/trunk/; revision=42

---
+++ src/DBGPerspective/nmv-proc-list-dialog.h
@@ -0,0 +1,56 @@
+//Author: Dodji Seketeli
+/*
+ *This file is part of the Nemiver project
+ *
+ *Nemiver is free software; you can redistribute
+ *it and/or modify it under the terms of
+ *the GNU General Public License as published by the
+ *Free Software Foundation; either version 2,
+ *or (at your option) any later version.
+ *
+ *Nemiver is distributed in the hope that it will
+ *be useful, but WITHOUT ANY WARRANTY;
+ *without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *See the GNU General Public License for more details.
+ *
+ *You should have received a copy of the
+ *GNU General Public License along with Goupil;
+ *see the file COPYING.
+ *If not, write to the Free Software Foundation,
+ *Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *See COPYRIGHT file copyright information.
+ */
+#ifndef __NMV_PROC_LIST_DIALOG_H__
+#define __NMV_PROC_LIST_DIALOG_H__
+
+#include "nmv-object.h"
+#include "nmv-safe-ptr-utils.h"
+#include "nmv-proc-mgr.h"
+
+namespace common {
+    class UString ;
+}
+
+namespace nemiver {
+class ProcListDialog : public Object {
+    //non copyable
+    ProcListDialog (const ProcListDialog &) ;
+    ProcListDialog& operator= (const ProcListDialog &) ;
+
+    ProcListDialog () ;
+
+public:
+
+    ProcListDialog (const UString &a_root_path) ;
+    virtual ~ProcListDialog () {}
+
+    gint run () ;
+    bool has_selected_process () ;
+    bool get_selected_process (IProcMgr::Process &a_proc/*out param*/) ;
+};//end class ProcListDialog
+}//end namespace nemiver
+
+#endif //__NMV_PROC_LIST_DIALOG_H__
+
