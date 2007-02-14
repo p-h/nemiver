977b99a6cee8da6372d09dceaf1607fc454ca2cb
Jonathon Jongsma <jjongsma@src.gnome.org>
2007-02-14 05:02:26 +0000

    Replace Goupil with Nemiver in a lot of header license blurbs
    svn path=/trunk/; revision=397

--- src/common/nmv-option-utils.h
+++ src/common/nmv-option-utils.h
@@ -14,7 +14,7 @@
  *See the GNU General Public License for more details.
  *
  *You should have received a copy of the
- *GNU General Public License along with Goupil;
+ *GNU General Public License along with Nemiver;
  *see the file COPYING.
  *If not, write to the Free Software Foundation,
  *Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
