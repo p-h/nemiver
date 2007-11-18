8c7261bb8163832ab57c570bd3ce451d01d5d93d
Dodji Seketeli <dodji@src.gnome.org>
2007-11-18 00:26:05 +0000

    added tests/test-var-walker.cc to test IVarWalker
    svn path=/trunk/; revision=603

--- tests/test-var-walker.cc
+++ tests/test-var-walker.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <boost/test/test_tools.hpp>
 #include <boost/test/minimal.hpp>
 #include <glibmm.h>
 #include "common/nmv-initializer.h"
@@ -17,16 +18,48 @@ IDebugger::Frame s_current_frame ;
 
 typedef std::list<IDebugger::VariableSafePtr> DebuggerVariableList ;
 typedef std::map<std::string, IVarListWalkerSafePtr> VarListWalkerMap ;
+typedef std::map<std::string, string> StringMap ;
 
-VarListWalkerMap s_var_list_walker ;
+VarListWalkerMap&
+var_list_walker ()
+{
+    static VarListWalkerMap s_var_list_walker ;
+    return s_var_list_walker ;
+}
+
+StringMap&
+expected_variables ()
+{
+    static StringMap s_expected_variables ;
+    return s_expected_variables ;
+}
+
+StringMap&
+actual_variables ()
+{
+    static StringMap s_actual_variables ;
+    return s_actual_variables ;
+}
+
+void on_variable_visited_signal (const IVarWalkerSafePtr &a_walker,
+                                 IDebuggerSafePtr &a_debugger) ;
+
+void on_variable_list_visited_signal (IDebuggerSafePtr &a_debugger) ;
 
 IVarListWalkerSafePtr
-create_var_list_walker ()
+create_var_list_walker (IDebuggerSafePtr a_debugger)
 {
+    THROW_IF_FAIL (a_debugger) ;
+
     IVarListWalkerSafePtr result  =
         DynamicModuleManager::load_iface_with_default_manager<IVarListWalker>
-                                                                ("varwalkerlist",
+                                                                ("varlistwalker",
                                                                  "IVarListWalker") ;
+    result->initialize (a_debugger) ;
+    result->variable_visited_signal ().connect
+        (sigc::bind (&on_variable_visited_signal, a_debugger)) ;
+    result->variable_list_visited_signal ().connect
+        (sigc::bind (&on_variable_list_visited_signal, a_debugger)) ;
     return result ;
 }
 
@@ -83,33 +116,11 @@ on_stopped_signal (const UString &a_reason,
     s_current_frame = a_frame ;
 
     MESSAGE ("stopped in function: '"
-                   << a_frame.function_name ()
-                   << "()'") ;
+             << a_frame.function_name ()
+             << "()'") ;
 
     a_debugger->list_local_variables () ;
     a_debugger->list_frames_arguments () ;
-
-    if (a_frame.function_name () == "main") {
-        if (s_var_list_walker.find ("main") == s_var_list_walker.end ()) {
-            s_var_list_walker["main"] = create_var_list_walker () ;
-        }
-        a_debugger->do_continue() ;
-    } else if (a_frame.function_name () == "func1") {
-        if (s_var_list_walker.find ("func1") == s_var_list_walker.end ()) {
-            s_var_list_walker["func1"] = create_var_list_walker () ;
-        }
-        a_debugger->do_continue() ;
-    } else if (a_frame.function_name () == "func2") {
-        if (s_var_list_walker.find ("func2") == s_var_list_walker.end ()) {
-            s_var_list_walker["func2"] = create_var_list_walker () ;
-        }
-        a_debugger->do_continue() ;
-    } else if (a_frame.function_name () == "func3") {
-        if (s_var_list_walker.find ("func3") == s_var_list_walker.end ()) {
-            s_var_list_walker["func3"] = create_var_list_walker () ;
-        }
-        a_debugger->do_continue() ;
-    }
 }
 
 void
@@ -124,9 +135,9 @@ on_frames_arguments_listed_signal
         LOG_ERROR ("Could not find current frame") ;
         return ;
     }
-    BOOST_REQUIRE (s_var_list_walker.find (s_current_frame.function_name ())
-                   != s_var_list_walker.end ()) ;
-    IVarListWalkerSafePtr walker = s_var_list_walker[s_current_frame.function_name ()];
+    BOOST_REQUIRE (var_list_walker ().find (s_current_frame.function_name ())
+                   != var_list_walker ().end ()) ;
+    IVarListWalkerSafePtr walker = var_list_walker ()[s_current_frame.function_name ()];
     BOOST_REQUIRE (walker) ;
     walker->remove_variables () ;
     walker->append_variables (it->second) ;
@@ -139,13 +150,34 @@ on_local_variables_listed_signal (const DebuggerVariableList &a_variables,
 {
     if (a_variables.empty () || a_cookie.empty ()) {
     }
-    BOOST_REQUIRE (s_var_list_walker.find (s_current_frame.function_name ())
-                   != s_var_list_walker.end ()) ;
-    IVarListWalkerSafePtr walker = s_var_list_walker[s_current_frame.function_name ()];
+    BOOST_REQUIRE (var_list_walker ().find (s_current_frame.function_name ())
+                   != var_list_walker ().end ()) ;
+    IVarListWalkerSafePtr walker = var_list_walker ()[s_current_frame.function_name ()];
     BOOST_REQUIRE (walker) ;
     walker->remove_variables () ;
     walker->append_variables (a_variables) ;
     walker->do_walk_variables () ;
+}
+
+void
+on_variable_visited_signal (const IVarWalkerSafePtr &a_walker,
+                            IDebuggerSafePtr &a_debugger)
+{
+    if (a_debugger) {}
+
+    MESSAGE ("in function "
+             << s_current_frame.function_name ()
+             << ", visited "
+             << a_walker->get_variable ()->name ()) ;
+    actual_variables ()[s_current_frame.function_name ()] +=
+        " " + a_walker->get_variable ()->name () ;
+}
+
+void
+on_variable_list_visited_signal (IDebuggerSafePtr &a_debugger)
+{
+    a_debugger->do_continue () ;
+    MESSAGE ("continuing from frame " << s_current_frame.function_name ()) ;
 }
 
 NEMIVER_API int
@@ -188,16 +220,50 @@ test_main (int argc, char **argv)
     debugger->set_breakpoint ("func1") ;
     debugger->set_breakpoint ("func2") ;
     debugger->set_breakpoint ("func3") ;
-    s_var_list_walker["main"] = create_var_list_walker () ;
-    s_var_list_walker["func1"] = create_var_list_walker () ;
-    s_var_list_walker["func2"] = create_var_list_walker () ;
-    s_var_list_walker["func3"] = create_var_list_walker () ;
+
+    var_list_walker ()["main"] = create_var_list_walker (debugger) ;
+    expected_variables ()["main"] = " person" ;
+    var_list_walker ()["func1"] = create_var_list_walker (debugger) ;
+    expected_variables ()["func1"] = " i" ;
+    var_list_walker ()["func2"] = create_var_list_walker (debugger) ;
+    expected_variables ()["func2"] = " j a_a a_b" ;
+    var_list_walker ()["func3"] = create_var_list_walker (debugger) ;
+    expected_variables ()["func3"] = " a_param" ;
     debugger->run () ;
 
     //****************************************
     //run the event loop.
     //****************************************
     s_loop->run () ;
+
+    BOOST_REQUIRE (actual_variables ().find ("main")  != actual_variables ().end ()) ;
+    BOOST_REQUIRE (actual_variables ().find ("func1") != actual_variables ().end ()) ;
+    BOOST_REQUIRE (actual_variables ().find ("func2") != actual_variables ().end ()) ;
+    BOOST_REQUIRE (actual_variables ().find ("func3") != actual_variables ().end ()) ;
+
+    if (actual_variables ()["main"] != expected_variables ()["main"]) {
+        string str ("main vars: ") ;
+        str += actual_variables ()["main"] ;
+        BOOST_FAIL (str.c_str ()) ;
+    }
+
+    if (actual_variables ()["func1"] != expected_variables ()["func1"]) {
+        string str ("func1 vars: ") ;
+        str += actual_variables ()["func1"] ;
+        BOOST_FAIL (str.c_str ()) ;
+    }
+
+    if (actual_variables ()["func2"] != expected_variables ()["func2"]) {
+        string str ("func2 vars: ") ;
+        str += actual_variables ()["func2"] ;
+        BOOST_FAIL (str.c_str ()) ;
+    }
+
+    if (actual_variables ()["func3"] != expected_variables ()["func3"]) {
+        string str ("func3 vars: ") ;
+        str += actual_variables ()["func3"] ;
+        BOOST_FAIL (str.c_str ()) ;
+    }
 
     NEMIVER_CATCH_AND_RETURN_NOX (-1)
 
