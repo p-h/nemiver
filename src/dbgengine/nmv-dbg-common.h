eff33b56ab76e864de791e1f5e97ed8e901fdc89
Dodji Seketeli <dodji@src.gnome.org>
2007-11-18 20:13:59 +0000

    Fix a potential refcounting issue.
    	* src/dbgengine/nmv-dbg-common.h:
    	  (Command::variable ()): Don't pass VariableSafePtr by reference
    	  otherwise we bypass its refcounting.
    
    svn path=/trunk/; revision=617

--- src/dbgengine/nmv-dbg-common.h
+++ src/dbgengine/nmv-dbg-common.h
@@ -81,7 +81,7 @@ public:
     const UString& tag1 () const {return m_tag1;}
     void tag1 (const UString &a_in) {m_tag1 = a_in;}
 
-    void variable (const IDebugger::VariableSafePtr &a_in) {m_var = a_in;}
+    void variable (const IDebugger::VariableSafePtr a_in) {m_var = a_in;}
     IDebugger::VariableSafePtr variable () const {return m_var;}
 
     /// @}
