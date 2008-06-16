02f29b7df88c8e95fffb69821c50443f654152e0
Jonathon Jongsma <jjongsma@src.gnome.org>
2008-06-16 14:11:57 +0000

    add a test which loops infinitely (e.g. a simple gtkmm program) to make it
    	* tests/Makefile.am:
    	* tests/gtkmm-test.cc: add a test which loops infinitely (e.g. a simple
    	gtkmm program) to make it easier to test state behaviors while the program
    	is running in an infinite loop
    
    svn path=/trunk/; revision=842

---
+++ tests/gtkmm-test.cc
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *
+ *  Copyright (c) 2008 Jonathon Jongsma
+ *
+ *  This file is part of nemiver
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ *******************************************************************************/
+
+#include <gtkmm.h>
+#include <iostream>
+
+void on_button_clicked ()
+{
+    std::cout << "Hello, World" << std::endl;
+}
+
+int main (int argc, char** argv)
+{
+    Gtk::Main kit (argc, argv);
+    Gtk::Window win;
+    Gtk::Button button ("Click Me");
+    button.signal_clicked ().connect (sigc::ptr_fun (on_button_clicked));
+    win.add (button);
+    win.show_all ();
+    Gtk::Main::run (win);
+    return 0;
+}
+
