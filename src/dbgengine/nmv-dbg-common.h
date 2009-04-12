2a43f1e49f16e38ea2345c4aad47f5d69c22e2f0
Dodji Seketeli <dodji@redhat.com>
2009-04-12 20:16:37 +0200

    Add a new integer tag to the Command type
    	* src/dbgengine/nmv-dbg-common.h:
    	(Command::tag2): new accessors.

--- src/dbgengine/nmv-dbg-common.h
+++ src/dbgengine/nmv-dbg-common.h
@@ -30,35 +30,43 @@ NEMIVER_BEGIN_NAMESPACE (nemiver)
 
 /// \brief A container of the textual command sent to the debugger
 class Command {
+    UString m_cookie ;
     UString m_name ;
-    UString m_value ;
+    UString m_value;
     UString m_tag0 ;
     UString m_tag1 ;
-    UString m_cookie ;
+    int m_tag2;
     IDebugger::VariableSafePtr m_var ;
 
 public:
 
-    Command ()  {clear ();}
+    Command ():
+      m_tag2 (0)
+    {
+        clear ();
+    }
 
     /// \param a_value a textual command to send to the debugger.
     Command (const UString &a_value) :
-        m_value (a_value)
+        m_value (a_value),
+        m_tag2 (0)
     {
     }
 
     Command (const UString &a_name, const UString &a_value) :
         m_name (a_name),
-        m_value (a_value)
+        m_value (a_value),
+        m_tag2 (0)
     {
     }
 
     Command (const UString &a_name,
              const UString &a_value,
              const UString &a_cookie) :
+        m_cookie (a_cookie),
         m_name (a_name),
         m_value (a_value),
-        m_cookie (a_cookie)
+        m_tag2 (0)
     {
     }
 
@@ -80,6 +88,9 @@ public:
 
     const UString& tag1 () const {return m_tag1;}
     void tag1 (const UString &a_in) {m_tag1 = a_in;}
+
+    int tag2 () const {return m_tag2;}
+    void tag2 (int a_in) {m_tag2 = a_in;}
 
     void variable (const IDebugger::VariableSafePtr a_in) {m_var = a_in;}
     IDebugger::VariableSafePtr variable () const {return m_var;}
